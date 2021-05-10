374683e4cd0f04ce690191e7642f463f2f1e49fa
Giordano Dìaz <diazgiordano@gmail.com>
2021-05-10 18:00:38 -0500

    Add test

--- src/__test__/Pokedex.test.js
+++ src/__test__/Pokedex.test.js
@@ -6,24 +6,18 @@ import { render, screen } from '@testing-library/react';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import CategoryTypes from '../containers/ByTypes';
+import Pokedex from '../containers/Pokedex';
 
 describe('Creates a component', () => {
   const middlewares = [thunk];
-  // const array = [{ name: 'normal', url: 'https://pokeapi.co/api/v2/type/1/' }, { name: 'fighting', url: 'https://pokeapi.co/api/v2/type/2/' }];
-  const initialState = {
-    categories: [],
-    loading: true,
-    fetchedCategories: {},
-    currentFilter: 'All',
-  };
+  const initialState = [];
   const mockStore = configureStore(middlewares);
   let store;
   it('renders correctly', () => {
     store = mockStore(initialState);
     const tree = renderer.create(
       <Provider store={store}>
-        <CategoryTypes />
+        <Pokedex />
       </Provider>,
     ).toJSON();
     expect(tree).toMatchSnapshot();
@@ -31,8 +25,8 @@ describe('Creates a component', () => {
 
   it('Renders a component', () => {
     store = mockStore(initialState);
-    render(<Provider store={store}><CategoryTypes /></Provider>);
-    const element = screen.getByText('Please Select a Type of Pokemon');
+    render(<Provider store={store}><Pokedex /></Provider>);
+    const element = screen.getByText('Pokemon');
     expect(element).toBeInTheDocument();
   });
 });
